8da7b5ca18a4fa0135ab752d35a0bc4f8e9bbcb8
roman-coding <mbizgle@gmail.com>
2023-01-24 18:14:02 +0300

    feat: add complete Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map((pair) => pair.join(''))
+  for (let i = str.length; i >= 0; i--) {
+    for (let j = 0; j < brackets.length; j++) {
+      if (str.includes(brackets[j])) {
+        str = str.replace(brackets[j], '')
+        j = 0
+      } else {
+        j++
+      }
+    }
+  }
+  return str.length === 0
 }
